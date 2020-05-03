d4ad80071e40da67082d9dd6b38594fe7a6572a9
danilobrinu <ddbn.c2@gmail.com>
2020-05-02 21:22:49 -0500

    Update scripts

--- infra/roles/create_instance/files/user_data.sh
+++ infra/roles/create_instance/files/user_data.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
-set -e -x
+
 sudo apt update -y && sudo apt install nginx -y
-
-# cat > /etc/nginx/sites-enabled/default << EOF
-# server {
-#     #listen   80; ## listen for ipv4; this line is default and implied
-#     #listen   [::]:80 default_server ipv6only=on; ## listen for ipv6
-#     listen 80;
-#     server_name www.capstion.io;
-#     root /var/www/html;
-#     index index.html index.htm;
-#     access_log /var/log/nginx/access.log;
-#     error_log /var/log/nginx/error.log;
-#     # Make site accessible from http://localhost/
-#     server_name localhost;
-#     location / {
-#     }
-# }
-# EOF
-
-# cat > /var/www/html/index.html << HEREDOC
-# <!DOCTYPE html>
-# <html>
-# <body>
-# <p style="color:blue">blue.</p>
-# </body>
-# </html>
-# HEREDOC
 
 echo '<p style="color:blue">blue.</p>' > /var/www/html/index.html
 
