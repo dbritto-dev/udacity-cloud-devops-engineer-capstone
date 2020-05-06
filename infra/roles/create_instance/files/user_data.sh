4bb7be7544318f8fce4460e121c473dbe2e56ebd
danilobrinu <ddbn.c2@gmail.com>
2020-05-06 17:26:04 -0500

    Minor fixes

--- infra/roles/create_instance/files/user_data.sh
+++ infra/roles/create_instance/files/user_data.sh
@@ -8,7 +8,7 @@ sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-p
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 sudo apt update -y && sudo apt install docker-ce docker-ce-cli containerd.io
