011c1ca7f8b0dce7aa9fbfc72ac09a92b94c657b
danilobrinu <ddbn.c2@gmail.com>
2020-05-06 17:17:48 -0500

    Minor changes

--- infra/roles/create_instance/files/user_data.sh
+++ infra/roles/create_instance/files/user_data.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-set -ex
 
 # Installing git
 sudo apt update -y && sudo apt install git -y
 
 # Installing docker
-sudo apt install
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common -y
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
