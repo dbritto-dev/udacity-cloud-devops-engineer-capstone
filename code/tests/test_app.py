a282babe1603d8108274bc3b0fa71008e6fe8485
danilobrinu <ddbn.c2@gmail.com>
2020-06-01 16:43:50 -0500

    Improve tests

--- code/tests/test_app.py
+++ code/tests/test_app.py
@@ -1,3 +1,6 @@
+# 3rd Party Packages
+from pytest import mark
+
 # Local Packages
 from capstone import create_app
 
@@ -7,25 +10,20 @@ JSON_MIMETYPE = "application/json"
 HTTP_STATUS_OK = 200
 
 
-def test_index():
+@mark.parametrize(
+    "valid_case",
+    [(None, "Hello World!"), ("", "Hello World!"), ("Docker App", "Hello Docker App!")],
+)
+def test_index(valid_case, monkeypatch):
+    value, expected_output = valid_case
+
     with app.test_client() as client:
+        monkeypatch.setenv("APP_NAME", value or "")
         res = client.get("/")
 
         assert res.status_code == HTTP_STATUS_OK
         assert res.mimetype == JSON_MIMETYPE
-        assert res.json.get("message") == "Hello World!"
-
-
-def test_index_with_app_name(monkeypatch):
-    with app.test_client() as client:
-        app_name = "Docker App"
-        monkeypatch.setenv("APP_NAME", app_name)
-
-        res = client.get("/")
-
-        assert res.status_code == HTTP_STATUS_OK
-        assert res.mimetype == JSON_MIMETYPE
-        assert res.json.get("message") == f"Hello {app_name}!"
+        assert res.json.get("message") == expected_output
 
 
 def test_world_stats():
